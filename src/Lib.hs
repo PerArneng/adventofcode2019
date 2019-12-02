d083a05c804264a675c57914b58d97a7c828f0e5
Per Arneng <per.arneng@scalebit.com>
2019-12-02 22:28:33 +0100

    added some code for day 1 part 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,15 @@ module Lib
     ( someFunc
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List
+
+calculateRequiredFuel :: Int -> Int
+calculateRequiredFuel moduleMass = (round ((fromIntegral moduleMass) / 3.0)) - 2
+        
+readModuleMasses :: String -> IO [String]
+readModuleMasses path =  
+    (readFile path) >>= pure . lines
+
+day1Part1 :: String -> IO Int
+day1Part1 path =
+    (readModuleMasses path) >>= map (read :: Int)
